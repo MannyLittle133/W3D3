ccd32485ca3a5e8d57e92e37f8c2878965ad5bbc
meihuang <meihuang8328@gmail.com>
2022-11-23 15:39:12 -0500

    continuation

--- recursion.rb
+++ recursion.rb
@@ -37,7 +37,7 @@ def exp(b, n)
 
 end
 
-p exp(2, 4)
+# p exp(2, 4)
 
 
 # def deep_dup(array) # [1, [2], [3, [4]]]
@@ -60,7 +60,7 @@ p exp(2, 4)
 
 # p b = deep_dup(robot_parts)
 # p b[1] << "LEDS"
-# p robot_parts
+# p robot_partsp 
 
 def fibonacci(n)
     return [] if n == 0
@@ -70,7 +70,50 @@ def fibonacci(n)
     fibonacci(n-1) << fibonacci(n-1)[-2] + fibonacci(n-1)[-1]
 end
 
-p fibonacci(6)
 
 
 
+def fibonacci(n)
+
+    return [] if n == 0
+    return [0] if n == 1
+    return [0, 1] if n == 2
+
+    arr = [0, 1]
+
+    (3..n).each do
+
+        arr << arr[-1] + arr[-2]
+
+    end
+
+    arr
+
+end
+
+
+def bsearch(array, target)
+
+    midpoint = array.length / 2
+
+    return nil if array.length <= 1 && array[0] != target
+
+    if array[midpoint] == target 
+        return midpoint
+    elsif array[midpoint] > target
+        bsearch(array[0...midpoint], target)
+    elsif array[midpoint] < target 
+        bsearch1 = bsearch(array[midpoint..-1], target)
+        bsearch1 == nil? nil : bsearch1 + midpoint
+    end
+
+end
+
+
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
